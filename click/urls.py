8cae0263ab14b151da9cfbd2dd118a77e74483bf
amjan <amjan91@gmail.com>
2023-04-12 10:51:05 -0400

    edit added to urls.py

--- click/urls.py
+++ click/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views  
    
 urlpatterns = [
-    path('listings_protected/', views.ListingsProtected.as_view(), name ='listings'),
-    path('listing_create/', views.ListingCreateProtected.as_view(), name='listing_create'),
+    path('click/', views.ListingsProtected.as_view(), name ='listings'),
+    path('click_create/', views.ListingCreateProtected.as_view(), name='listing_create'),
+    path('click_edit/<uuid:pk>/', views.ListingDetailProtected.as_view(), name='listing_edit'),
 ]
