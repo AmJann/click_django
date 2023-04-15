9aacf8be49739a7391dc5fb9adbdfe55b56f28fc
amjan <amjan91@gmail.com>
2023-04-14 23:43:56 -0400

    sigh

--- click_django/settings.py
+++ click_django/settings.py
@@ -36,7 +36,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True if os.environ['MODE'] == 'dev' else False
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
